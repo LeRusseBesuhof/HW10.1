fd581b840689087c0d8e2e35ab6e327ff69e535a
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-04-29 16:07:14 +0700

    Initial Commit

---
+++ HW10.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10.1
+//
+//  Created by Павел Градов on 29.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
